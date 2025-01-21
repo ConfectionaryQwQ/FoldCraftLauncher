d2da4c719ac6b7d0d5078195ba2cc209189e98a4
Confectionary <91813030+ConfectionaryQwQ@users.noreply.github.com>
2025-01-21 18:11:19 +0800

    Update egl_bridge.c

--- FCLauncher/src/main/jni/pojav/egl_bridge.c
+++ FCLauncher/src/main/jni/pojav/egl_bridge.c
@@ -103,7 +103,7 @@ EXTERNAL_API void* pojavGetCurrentContext() {
 bool checkAdrenoGraphics() {
     EGLDisplay eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     if(eglDisplay == EGL_NO_DISPLAY || eglInitialize(eglDisplay, NULL, NULL) != EGL_TRUE) return false;
-    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_SURFACE_TYPE, EGL_PBUFFER_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE };
+    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_SURFACE_TYPE, EGL_PBUFFER_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT, EGL_NONE };
     EGLint num_configs = 0;
     if(eglChooseConfig(eglDisplay, egl_attributes, NULL, 0, &num_configs) != EGL_TRUE || num_configs == 0) {
         eglTerminate(eglDisplay);
